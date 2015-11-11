7e8e6702dfbe4d86a4516f1080074af6c9a13add
Dave @ DevedUp <email@devedup.com>
2015-11-11 11:34:43 +0000

    Moved the model generator classes out of the flickrkit sources classes path

--- ModelGenerator/SupportingFiles/main.m
+++ ModelGenerator/SupportingFiles/main.m
@@ -7,10 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+#import "FKAppDelegate.h"
 
+@class FKAppDelegate;
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FKAppDelegate class]));
     }
 }
