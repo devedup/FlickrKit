1735e324e764cd610c8a8ce89acd75b894c9903e
DaveC @ DevedUp <email@devedup.com>
2016-06-30 21:55:31 +0100

    GalleryrPro2-1-14-quickfix

--- Classes/DevedUpKit/FKDUNetworkController.h
+++ Classes/DevedUpKit/FKDUNetworkController.h
@@ -5,13 +5,6 @@
 //  Created by David Casserly on 05/06/2013.
 //  Copyright (c) 2013 DevedUp Ltd. All rights reserved. http://www.devedup.com
 
-typedef enum {
-	HTTPMethodGET = 0,
-	HTTPMethodPOST,
-	HTTPMethodPUT,
-	HTTPMethodDELETE
-} HTTPMethod;
-
 @class DUNetworkRequestOperation;
 
 @interface FKDUNetworkController : NSObject
